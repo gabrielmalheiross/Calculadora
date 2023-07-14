c0c23a94900480936c9f218471cbd24d814d446a
Gabriel Malheiros <gebrielmalheiros1515@gmail.com>
2023-07-14 11:05:48 -0300

    calc: correcao geral

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
-function insert(num)
-{
+function valor(num) {
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
 }
+
+function apagar() {
+    document.getElementById('resultado').innerHTML = null;
+}
+
+function back() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function calcular()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado)
+    {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else
+    {
+        document.getElementById('resultado').innerHTML = "Nada..."
+    }
+}
+
+// function calcular() {
+//     var numero = document.getElementById('resultado').value;
+//     var op = document.getElementById('resultado').value;
+
+//     switch (op) {
+//         case '+':
+//             numero = numero + numero;
+//             numero.innerHTML = numero;
+//         case '-':
+//             numero = numero - numero;
+//             return numero;
+//         case '*':
+//             numero = numero * numero;
+//             return numero;
+//         case '/':
+//             numero = numero / numero;
+//             return numero;
+//         default:
+//             break;
+//     }
+// }
